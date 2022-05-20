e7145f4b62a0af3c4c5bf4e1e4220ac0acadf259
Tim <timothycogan@gmail.com>
2022-05-20 10:18:32 -0500

    Improve variable names

--- lz77/lz77.py
+++ lz77/lz77.py
@@ -14,10 +14,10 @@ def compress(data: bytes) -> bytes:
     i = 0
     while i < len(data):
         if match := find_longest_match(data, i):
-            best_distance, best_length = match
+            match_distance, match_length = match
             output_buffer.append(IS_MATCH_BIT)
-            output_buffer.frombytes(bytes([best_distance >> 4, ((best_distance & 0xF) << 4) | best_length]))
-            i += best_length
+            output_buffer.frombytes(bytes([match_distance >> 4, ((match_distance & 0xF) << 4) | match_length]))
+            i += match_length
         else:
             output_buffer.append(not IS_MATCH_BIT)
             output_buffer.frombytes(bytes([data[i]]))
@@ -34,6 +34,7 @@ def decompress(compressed_bytes: bytes) -> bytes:
 
     output_buffer = []
 
+    # Anything less than 9 bits is padding
     while len(data) >= 9:
         if data.pop(0) != IS_MATCH_BIT:
             byte = data[:8].tobytes()
@@ -52,16 +53,16 @@ def decompress(compressed_bytes: bytes) -> bytes:
 
 def find_longest_match(data: bytes, current_position: int) -> Optional[Tuple[int, int]]:
     end_of_buffer = min(current_position + MATCH_LENGTH_MASK, len(data))
-    start_index = max(0, current_position - WINDOW_SIZE)
+    search_start = max(0, current_position - WINDOW_SIZE)
 
     # Optimization: Only consider substrings of length 2 and greater, and just
     # output any substring of length 1 (8 bits uncompressed is better than 13 bits
     # for the flag, distance, and length)
-    for j in range(end_of_buffer, current_position + 3, -1):
-        substring = data[current_position:j]
-        for i in range(start_index, current_position):
-            if substring == get_wrapped_slice(data[i:current_position], len(substring)):
-                return current_position - i, len(substring)
+    for match_candidate_end in range(end_of_buffer, current_position + 3, -1):
+        match_candidate = data[current_position:match_candidate_end]
+        for search_position in range(search_start, current_position):
+            if match_candidate == get_wrapped_slice(data[search_position:current_position], len(match_candidate)):
+                return current_position - search_position, len(match_candidate)
 
 
 def get_wrapped_slice(x: bytes, num_bytes: int) -> bytes:
